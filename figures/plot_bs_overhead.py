621b4e4a9f459b8884c1c27c580989fc905a0ea3
Jiaqi Yan@IIT-PC <littlepretty881203@gmail.com>
2017-01-06 22:03:09 -0600

    overhead runtime plot
    Signed-off-by: Jiaqi Yan@IIT-PC <littlepretty881203@gmail.com>

---
+++ figures/plot_bs_overhead.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.rc('font', size=18)
+
+data = np.loadtxt('bs_overhead.txt')
+
+x = data[:, 2]
+y = data[:, 3]
+logx = np.log2(data[:, 2])
+logy = np.log2(data[:, 3])
+
+print(x)
+print(y)
+
+fig, ax = plt.subplots()
+ax.plot(logx, logy, 'b', linewidth=4.0, label='Binary logarithm')
+width = 0.4
+ax.bar(logx - width/2, logy, width, color='white', hatch='//')
+
+plt.xticks(logx, ['%.1f' % i for i in logx])
+plt.grid(True)
+
+plt.xlabel('#Rules processed in log scale')
+plt.ylabel('Runtime in log scale (Milliseconds)')
+plt.legend(loc='upper left')
+plt.tight_layout()
+
+plt.savefig('bs_overhead.eps', fmt='eps')
+plt.show()
