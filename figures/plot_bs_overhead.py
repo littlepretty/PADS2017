7a451aea604fdab2247482d118b149ae32b1ed01
Jiaqi Yan@IIT-PC <littlepretty881203@gmail.com>
2017-01-11 09:52:59 -0600

    Add conclusion
    Signed-off-by: Jiaqi Yan@IIT-PC <littlepretty881203@gmail.com>

--- figures/plot_bs_overhead.py
+++ figures/plot_bs_overhead.py
@@ -15,17 +15,22 @@ print(x)
 print(y)
 
 fig, ax = plt.subplots()
+
+# ax.loglog(x, y, linewidth=3.0, marker='>', markersize=10.0)
+# plt.grid(True)
+# plt.xticks(x, ['%d' % i for i in x])
+# plt.xlabel('#Rules Processed')
+# plt.ylabel('Running Time (Milliseconds)')
+
 ax.plot(logx, logy, 'b', linewidth=4.0, label='Binary logarithm')
 width = 0.4
 ax.bar(logx - width/2, logy, width, color='white', hatch='/')
-
 plt.xticks(logx, ['%.1f' % i for i in logx])
 plt.grid(True)
-
 plt.xlabel('$\log_2$(#Rules Processed)')
 plt.ylabel('$\log_2$(Runtime) (Milliseconds)')
 plt.legend(loc='upper left')
-plt.tight_layout()
 
+plt.tight_layout()
 plt.savefig('bs_overhead.eps', fmt='eps')
 plt.show()
